106c607695f4c7eb447dd16b11b2b68e9c53b59b
OKrylov <krylov92oa@gmail.com>
2018-06-08 22:37:24 +0300

    Lesson 6

---
+++ Lesson_6/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Tree theTree = new TreeImpl();
+        theTree.insert(new Person("Oleg", 25, 60));
+        theTree.insert(new Person("Oleg", 25, 25));
+        theTree.insert(new Person("Oleg", 25, 66));
+        theTree.insert(new Person("Oleg", 25, 15));
+        theTree.insert(new Person("Oleg", 25, 45));
+        theTree.insert(new Person("Oleg", 25, 5));
+        theTree.insert(new Person("Oleg", 25, 20));
+        theTree.insert(new Person("Oleg", 25, 30));
+        theTree.insert(new Person("Oleg", 25, 55));
+        theTree.insert(new Person("Oleg", 25, 31));
+        theTree.insert(new Person("Oleg", 25, 62));
+
+//        theTree.remove(55);
+//        theTree.remove(62);
+//        System.out.println(theTree.remove(25));
+//        System.out.println("55 person = " + theTree.find(60));
+
+        theTree.traverse(TraverseType.PRE_ORDER);
+
+        theTree.displayTree();
+    }
+}
---
+++ Lesson_6/src/Tree.java
@@ -0,0 +1,13 @@
+public interface Tree {
+
+    void insert(Person value);
+
+    boolean remove(Person value);
+    boolean remove(int key);
+
+    Person find(int key);
+
+    void displayTree();
+
+    public void traverse(TraverseType type);
+}
